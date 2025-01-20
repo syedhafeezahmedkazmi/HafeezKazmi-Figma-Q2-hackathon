7e5aa17bb12aad10def1d7e6d2981ffbbb4745f0
syedhafeezahmedkazmi <121306021+syedhafeezahmedkazmi@users.noreply.github.com>
2025-01-20 21:05:19 +0500

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "./components/footer";
-import Header from "./components/header";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+       
         {children}
-        <Footer />
+       
         </body>
     </html>
   );
